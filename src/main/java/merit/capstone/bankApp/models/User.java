890e87b72302c54c913b1dcc8fe05338aaa53fdc
rekuch-nick <45768867+rekuch-nick@users.noreply.github.com>
2020-06-07 10:21:53 -0500

    minor doc add

--- src/main/java/merit/capstone/bankApp/models/User.java
+++ src/main/java/merit/capstone/bankApp/models/User.java
@@ -6,6 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * 
+ * This model class represents an individual bank customer or employee
+ * 
+ * Each Bank Account object should only be accessible through its user
+ *
+ */
 @Entity
 public class User {
 	
