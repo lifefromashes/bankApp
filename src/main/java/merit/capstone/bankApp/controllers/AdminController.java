c3df54dad1d92c01d3766ccf6e44fea0c15cad0c
rekuch-nick <45768867+rekuch-nick@users.noreply.github.com>
2020-06-09 08:48:01 -0500

    removed bank account stub

--- src/main/java/merit/capstone/bankApp/controllers/AdminController.java
+++ src/main/java/merit/capstone/bankApp/controllers/AdminController.java
@@ -1,5 +1,48 @@
 package merit.capstone.bankApp.controllers;
 
-public class AdminController {
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import merit.capstone.bankApp.models.User;
+
+@RestController
+public class AdminController {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass() );
+	
+	@Autowired private User userRepository;
+	
+	
+	
+	
+	@CrossOrigin
+	@PostMapping("Admin/NewCustomer")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User createCustomer( @RequestBody @Valid User user) {
+		
+		
+		
+		
+		
+		
+		userRepository.save(user);
+		
+		
+		return user;
+	}
+	
+	
+	
+	
 }
