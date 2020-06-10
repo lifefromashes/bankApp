ca93c2119e67a72c43ae780bcfedb7b207ec7518
rekuch-nick <45768867+rekuch-nick@users.noreply.github.com>
2020-06-10 11:38:58 -0500

    minor formatting

--- src/main/java/merit/capstone/bankApp/controllers/LoginController.java
+++ src/main/java/merit/capstone/bankApp/controllers/LoginController.java
@@ -1,5 +1,16 @@
 package merit.capstone.bankApp.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class LoginController {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass() );
+	
+	
 
 }
--- src/main/java/merit/capstone/bankApp/models/BankUser.java
+++ src/main/java/merit/capstone/bankApp/models/BankUser.java
@@ -28,27 +28,16 @@ public class BankUser {
 	private long id;
 
 	private String username;
-
 	private String password;
-
 	private String authority;
-
 	private String firstName;
-
 	private String lastName;
-
 	private String ssn;
-
 	private String email;
-
 	private String phone;
-
 	private String address;
-
 	private String city;
-
 	private String state;
-
 	private String zip;
 	
 	@OneToMany(cascade = CascadeType.ALL)
@@ -129,117 +118,34 @@ public class BankUser {
 	// getAvailBalance()
 	// updateContactInfo()
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getAuthority() {
-		return authority;
-	}
-
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getSsn() {
-		return ssn;
-	}
-
-	public void setSsn(String ssn) {
-		this.ssn = ssn;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	public List<BankAccount> getBankAccounts() {
-		return bankAccounts;
-	}
-
-	public void setBankAccounts(List<BankAccount> bankAccounts) {
-		this.bankAccounts = bankAccounts;
-	}
+	public long getId() { return id; }
+	public void setId(long id) { this.id = id; }
+	public String getUsername() { return username; }
+	public void setUsername(String username) { this.username = username; }
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+	public String getAuthority() { return authority; }
+	public void setAuthority(String authority) { this.authority = authority; }
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String firstName) { this.firstName = firstName; }
+	public String getLastName() { return lastName; }
+	public void setLastName(String lastName) { this.lastName = lastName; }
+	public String getSsn() { return ssn; }
+	public void setSsn(String ssn) { this.ssn = ssn; }
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+	public String getPhone() { return phone; }
+	public void setPhone(String phone) { this.phone = phone; }
+	public String getAddress() { return address; }
+	public void setAddress(String address) { this.address = address; }
+	public String getCity() { return city; }
+	public void setCity(String city) { this.city = city; }
+	public String getState() { return state; }
+	public void setState(String state) { this.state = state; }
+	public String getZip() { return zip; }
+	public void setZip(String zip) { this.zip = zip; }
+	public List<BankAccount> getBankAccounts() { return bankAccounts; }
+	public void setBankAccounts(List<BankAccount> bankAccounts) { this.bankAccounts = bankAccounts; }
 
 
 
